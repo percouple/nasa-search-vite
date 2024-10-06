b6b27af81688da4417484890a3f6ca05daeb5af9
Caleb Evans <calebsevan@gmail.com>
2024-10-05 22:18:22 -0500

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import {Title} from "./components/title-bar/Title";
+import { Title } from "./components/title-bar/Title";
 import { useState } from "react";
 import axios from "axios";
 import ContentDisplay from "./components/content-section/ContentDisplay";
-import './index.css'
+import "./index.css";
 
-const initialNasaData = [];
+const initialNasaData: object[] = [];
 
 function App() {
   const [nasaData, setNasaData] = useState(initialNasaData);
   const [imageData, setImageData] = useState([]);
-  const [inputValue, setInputValue] = useState<string>("nebula");
-  const [loading, setLoading] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>("");
   const [totalHits, setTotalHits] = useState<number>(0);
 
   const url = "https://images-api.nasa.gov/search";
@@ -22,42 +21,31 @@ function App() {
   // Input value change handler
   const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (inputValue !== ''){
-
-      setLoading(true);
+    if (inputValue !== "") {
+      setNasaData([]);
       axios
-      .get(`${url}?${params.toString()}`)
-      .then((res) => {
-        const { collection } = res.data;
-        setNasaData(collection);
-        setImageData(collection.items);
-        setTotalHits(collection.metadata.total_hits);
-        setTimeout(() => {
-          setLoading(false);
-        }, 2000);
-      })
-      .catch((err) => {
-        console.log(err);
-        setTimeout(() => {
-          setLoading(false);
-        }, 2000);
-      });
+        .get(`${url}?${params.toString()}`)
+        .then((res) => {
+          const { collection } = res.data;
+          setNasaData(collection);
+          setImageData(collection.items);
+          setTotalHits(collection.metadata.total_hits);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
-  }
-    
+  };
+
   return (
-    <div className={`${loading ? "loading" : ""} source-code-pro-normal`}>
+    <div className={`source-code-pro-normal`}>
       <Title
         totalHits={totalHits}
         setInputValue={setInputValue}
         inputValue={inputValue}
         submitForm={submitForm}
       />
-      <ContentDisplay
-        imageData={imageData}
-        setLoading={setLoading}
-        loading={loading}
-      />
+      <ContentDisplay imageData={imageData} />
     </div>
   );
 }
