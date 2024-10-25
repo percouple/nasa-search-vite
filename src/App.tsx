ba596717f7640afcbc8806f4da6c63c1bc4d7ecc
Caleb Evans <calebsevan@gmail.com>
2024-10-24 20:22:42 -0500

    fix re-render on window re-size

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,69 @@
 import { Title } from "./components/title-bar/Title";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import ContentDisplay from "./components/content-section/ContentDisplay";
 import Footer from "./components/footer/footer";
 import "./index.css";
 
+
 function App() {
   const [imageData, setImageData] = useState([]);
   const [inputValue, setInputValue] = useState<string>("");
   const [totalHits, setTotalHits] = useState<number>(0);
-
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  
   const url = "https://images-api.nasa.gov/search";
   const params = new URLSearchParams({
     description: inputValue,
     media_type: "image",
   });
 
+  const sendGetRequest = (e: React.FormEvent<HTMLFormElement>) => {
+    if (e) {
+      e.preventDefault();
+    }
+    if (inputValue !== "") {
+      axios
+      .get(`${url}?${params.toString()}`)
+      .then((res) => {
+        const { collection } = res.data;
+        setImageData(collection.items);
+        setTotalHits(collection.metadata.total_hits);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+  }
+
   // Input value change handler
   const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (inputValue !== "") {
-      // setNasaData([]);
-      axios
-        .get(`${url}?${params.toString()}`)
-        .then((res) => {
-          const { collection } = res.data;
-          setImageData(collection.items);
-          setTotalHits(collection.metadata.total_hits);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
+    sendGetRequest(e);
   };
+
+  useEffect(() => {
+    // @ts-expect-error *** No input necessary, and exception is handled in sendGetRequest itself
+    sendGetRequest(null); 
+  }, [windowSize])
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup on unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <div className="source-code-pro-normal flex-column">
