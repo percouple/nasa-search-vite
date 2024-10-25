28483da0ad6e789324f433642d426c9656979718
Caleb Evans <calebsevan@gmail.com>
2024-10-24 21:39:46 -0500

    styling fixes

--- src/components/content-section/card-select-screen/CardSelectScreen.tsx
+++ src/components/content-section/card-select-screen/CardSelectScreen.tsx
@@ -1,3 +1,4 @@
+import "./card-select-screen.css";
 
 const dateFixer = (date: Date) => {
   const input = new Date(date);
@@ -9,27 +10,43 @@ export default function CardSelectScreen({
   selectedCard,
   setSelectedCard,
 }) {
-
   const screenClickHandler = () => {
-    setSelectedCard(null)
-  }
+    setSelectedCard(null);
+  };
+
+  const openImageInNewTab = (imageUrl: string) => {
+    const newTab = window.open();
+    if (newTab) {
+      newTab.opener = null; // Prevents the new tab from accessing the original window
+      newTab.location = imageUrl; // Set the location of the new tab to the image URL
+    }
+  };
 
   return (
-      <div onClick={screenClickHandler} className="card-select-screen">
-        <div className="image-card">
-          <img src={imageData[selectedCard].links[0].href}></img>
-          <div className="image-card-text">
-            <div style={{ padding: "10px" }}>
-              <h4>
-                [{dateFixer(imageData[selectedCard].data[0].date_created)}]{" "}
-              </h4>
-              <h1 style={{ paddingTop: "10px", textDecorationLine: "underline" }}>{imageData[selectedCard].data[0].title}</h1>
-            </div>
-            <div style={{ padding: "10px" }}>
-              {imageData[selectedCard].data[0].description}
-            </div>
+    <div
+      onClick={screenClickHandler}
+      className="card-select-screen flex-center"
+    >
+      <div className="image-card">
+        <div className="flex-center">
+          <img
+            src={imageData[selectedCard].links[0].href}
+            className="responsive-image"
+            onClick={() => openImageInNewTab(imageData[selectedCard].links[0].href)}
+          ></img>
+          <div style={{ padding: "10px" }}>
+            <h2 style={{ paddingTop: "10px", fontWeight: "lighter" }}>
+              {imageData[selectedCard].data[0].title}
+            </h2>
+            <h4>
+              [{dateFixer(imageData[selectedCard].data[0].date_created)}]{" "}
+            </h4>
           </div>
         </div>
+          <div style={{ padding: "1rem", margin: "1rem" }}>
+            {imageData[selectedCard].data[0].description}
+          </div>
       </div>
+    </div>
   );
 }
